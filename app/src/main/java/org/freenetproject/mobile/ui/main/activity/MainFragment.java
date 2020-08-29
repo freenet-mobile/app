f6f902078fbbaf56f9ddaff0700364ed545f0d82
DC* <des@riseup.net>
2020-08-28 23:20:15 -0300

    Bundle sharesite plugin with the app

--- app/src/main/java/org/freenetproject/mobile/ui/main/activity/MainFragment.java
+++ app/src/main/java/org/freenetproject/mobile/ui/main/activity/MainFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
